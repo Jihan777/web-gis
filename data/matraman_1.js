d4bc01ebc374b76395732659bc81152aad7dcff3
Jihan777 <75110243+Jihan777@users.noreply.github.com>
2020-11-27 17:04:37 +0700

    Add files via upload

---
+++ data/matraman_1.js
@@ -0,0 +1,8 @@
+var json_matraman_1 = {
+"type": "FeatureCollection",
+"name": "matraman_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 106.872290212586663, -6.20640851438901 ] } }
+]
+}
